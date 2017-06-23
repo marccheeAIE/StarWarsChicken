9cc70b8489cc383ccaf9802af48d91fd47105808
AIE\marcc <marcc@aie.edu.au>
2017-06-23 10:30:47 +1000

    Begun AI Branch . . . started AI scripting

---
+++ Assets/AIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIScript : MonoBehaviour {
+
+    int AINumber = 9000;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AINumber++;
+	}
+}
